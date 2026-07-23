414e425c63d68ceb183b8fcfefbf4f279e0f045f
Kanhaiya Kumar <kanhaiyakumar993197@gmail.com>
2026-07-24 01:54:40 +0530

    Configure @EnableJpaRepositories and @EntityScan for base package structure

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,11 +2,15 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"app", "service", "repository"})
+@EnableJpaRepositories(basePackages = "repository")
+@EntityScan(basePackages = "domain")
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
